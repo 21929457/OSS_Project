a57c517015fd4b4800da2d626d639cdac0a78490
dhLee <apple855005@gmail.com>
2023-05-12 16:31:28 +0900

    add main.h

--- OSS_Project/main.c
+++ OSS_Project/main.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
+#include "main.h"
 
 int main(void) {
-	int temp,mode = 0;
-	
+
+	int temp,mode,back = 0;
+
 	do {
-		/* 초반 메뉴 선택 부분*/
-		scanf("%d", temp);
+		void startMenu();
+		scanf("%d", &temp);
 
 		if (temp == 1) {
-			printf("1.Easy Mode");
-			printf("2.Hard Mode");
-			printf("3.return to menu");
-			scanf("%d", mode);
-
-			if (mode == 1) {
-				/*이지모드로 게임 연결*/
-			}
-			else if(mode == 2) {
-				/*하드모드로 게임 연결*/
-			}
-			else if (mode == 3) {
-				/*초반 메뉴 선택부분*/
-				continue;
-			}
+			void selectMode();
+			scanf("%d", &mode);
+			temp = 3;
 		}
+		else  if(temp == 2) {
+			/*게임 설명*/
+			printf("돌아가기 (1)\n");
+			scanf("%d" , back);
+			}
 	} while (temp != 3);
+
+
+
+
+
+	if (mode == 1) {
+		/*이지모드로 게임 연결*/
+		printf("a");
+	}
+	else if(mode == 2) {
+		/*하드모드로 게임 연결*/
+		printf("b");
+	}
+	else if (mode == 3) {
+		/*초반 메뉴 선택부분*/
+		printf("c");
+			}
 }
